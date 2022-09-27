b87f6731a621e4896d88af9f44f61a845c5499dd
Saqlain <iamkhatik786@gmail.com>
2022-09-27 19:25:00 +0530

    handle exception

--- TestMoodAnalyzer.java
+++ TestMoodAnalyzer.java
@@ -4,19 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-class TestMoodAnalyzer {
+public class TestMoodAnalyzer {
 
 	@Test
 	public void givenMessage_whenSad_shouldReturnSad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.analyzeMood("This is a sad message");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+		String mood = moodAnalyzer.analyzeMood();
 		assertEquals("SAD", mood);
 	}
 	
 	@Test
 	public void givenMessage_whenNotSad_shouldReturnHappy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.analyzeMood("This is a happy message");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+		String mood = moodAnalyzer.analyzeMood();
+		assertEquals("HAPPY", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenNull_shouldThrowException() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+		String mood = moodAnalyzer.analyzeMood();
 		assertEquals("HAPPY", mood);
 	}
 }
