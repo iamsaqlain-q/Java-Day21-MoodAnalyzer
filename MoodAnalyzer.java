941aa48ac8f3fff30e52d3421962f5165bed1475
Saqlain Qureshi <iamkhatik786@gmail.com>
2022-09-27 19:43:49 +0530

    Inform user if entered Invalid Mood

---
+++ MoodAnalyzer.java
@@ -0,0 +1,29 @@
+package com.bl.moodanalyzer;
+
+import com.bl.moodanalyzer.MoodAnalysisException.ExceptionType;
+
+public class MoodAnalyzer {	
+	private String message;
+	
+	public MoodAnalyzer() {
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyzeMood() throws MoodAnalysisException {
+		try {
+			if(message.length() == 0) {
+				throw new MoodAnalysisException(ExceptionType.ENTERED_EMPTY, "You entered an Invalid input!\\nPlease enter a non-empty input for mood.");
+			}
+				
+			if(message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException(ExceptionType.ENTERED_NULL, "You entered an Invalid input!\\nPlease enter a non-NULL input for mood.");
+		}
+	}	
+}
