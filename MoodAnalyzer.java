3ca7b3b3e5efede1edd59d9ca8e968b63bb43064
Saqlain Qureshi <iamkhatik786@gmail.com>
2022-09-27 19:25:51 +0530

    handle exception

---
+++ MoodAnalyzer.java
@@ -0,0 +1,23 @@
+package com.bl.moodanalyzer;
+
+public class MoodAnalyzer {	
+	private String message;
+	
+	public MoodAnalyzer() {
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyzeMood() {
+		try {
+			if(message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}	
+}
