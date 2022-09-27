1d1bce6eeef59d48d154cbf76ab71644ffdced9a
Saqlain <iamkhatik786@gmail.com>
2022-09-27 19:18:19 +0530

    analyze mood

--- TestMoodAnalyzer.java
+++ TestMoodAnalyzer.java
@@ -4,19 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class TestMoodAnalyzer {
+class TestMoodAnalyzer {
 
 	@Test
 	public void givenMessage_whenSad_shouldReturnSad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
-		String mood = moodAnalyzer.analyzeMood();
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String mood = moodAnalyzer.analyzeMood("This is a sad message");
 		assertEquals("SAD", mood);
 	}
 	
 	@Test
 	public void givenMessage_whenNotSad_shouldReturnHappy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
-		String mood = moodAnalyzer.analyzeMood();
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String mood = moodAnalyzer.analyzeMood("This is a happy message");
 		assertEquals("HAPPY", mood);
 	}
 }
